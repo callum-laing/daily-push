89cbace782bce115f6fe32d71bfa2c3427f2a5de
Callum Laing <callum.laing91@hotmail.co.uk>
2025-05-29 22:26:17 +0100

    added dummy text

--- pages/create.vue
+++ pages/create.vue
@@ -2,6 +2,20 @@
 
 <template>
 	<h2>Create a task.</h2>
+
+	<form action="">
+		<label for="">Date:</label>
+		<input type="date" />
+
+		<label for="">Wins:</label>
+		<input type="text" />
+
+		<label for="">Struggles:</label>
+		<input type="text" />
+
+		<label for="">Notes:</label>
+		<input type="text" />
+	</form>
 </template>
 
 <style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,16 @@
 
 <template>
 	<h1>Welcome to my accountability tracker.</h1>
+
+	<div class="content">
+		<h4>Date: 29/05/25</h4>
+		<p>Wins: Started my first Nuxt project!</p>
+		<p>Struggles: Struggled starting my first Nux project!</p>
+		<p>
+			Notes: Got off to a good start at least.. the files are setup, my README is done and I am
+			feeling confident.
+		</p>
+	</div>
 </template>
 
 <style scoped></style>
