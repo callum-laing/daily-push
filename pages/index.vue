0aa2ea8b65f192fc00efd255ade859b860ee1d0b
Callum Laing <callum.laing91@hotmail.co.uk>
2025-05-31 21:26:32 +0100

    added a dummy article, tweaked wording

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,27 @@
 <script setup></script>
 
 <template>
-	<h1>Welcome to my accountability tracker.</h1>
+	<h2>Blogging my journey through Web Development.</h2>
 
 	<div class="content">
-		<h4>Date: 29/05/25</h4>
-		<p>Wins: Started my first Nuxt project!</p>
-		<p>Struggles: Struggled starting my first Nux project!</p>
-		<p>
-			Notes: Got off to a good start at least.. the files are setup, my README is done and I am
-			feeling confident.
-		</p>
+		<article class="day">
+			<h4>Date: 31.05.25</h4>
+			<p>
+				Wins: I've managed to get the bare bones setup so far, create post/admin login will be
+				linked together with Auth eventually.
+			</p>
+			<p>Struggles: Finding the time to sit and code for an hour has been challenging.</p>
+			<p>Notes: N/A</p>
+		</article>
+		<article class="day">
+			<h4>Date: 29/05/25</h4>
+			<p>Wins: Started my first Nuxt project!</p>
+			<p>Struggles: Struggled starting my first Nux project!</p>
+			<p>
+				Notes: Got off to a good start at least.. the files are setup, my README is done and I am
+				feeling confident.
+			</p>
+		</article>
 	</div>
 </template>
 
